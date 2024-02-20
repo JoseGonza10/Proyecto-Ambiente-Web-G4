457e971894714361eb03e0842e6a4a9175969a59
JoseGonza10 <123215301+JoseGonza10@users.noreply.github.com>
2024-02-19 19:33:18 -0600

    Commit 19/02/2024

---
+++ view/bienvenida.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=devie-width,intial-scale=1.0">
+        <title>Pagina web 1</title>
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Acerca de nosotros</li>
+            </ul>
+            <header>
+                <h1>¡Bienvenido!</h1>
+                <h2>Colegios - Empresas - Amigos - Familiares</h2>
+                <h3>El juego mas popular de USA, ahora el mejor y + grande de Paintball en C.R</h3>
+</header>
+</nav>
+</body>
+        </html>
